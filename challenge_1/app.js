f289e01b2b168549d9d142d2c9c2e728cd24459e
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-09-14 10:37:52 -0700

    Implement Basic File Setup for TicTacToe

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,11 @@
+//TEST
+// onsole.log("Hello, World! -Love, Jacob");
 
+function myTest() {
+  console.log('It Is working here!');
+  document.getElementById("test").innerHTML = "Hola, Mundo!";
+};
+
+//Invoke the myTest function
+myTest();
 
