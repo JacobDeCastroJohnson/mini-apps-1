72e5e113b5efd5d3f3dab9008904edbcf1d80813
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-09-14 11:13:31 -0700

    Implement Refresh button event listening test

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,4 +1,4 @@
-//TEST
+//============== TEST =====================
 // onsole.log("Hello, World! -Love, Jacob");
 
 function myTest() {
@@ -9,3 +9,24 @@ function myTest() {
 //Invoke the myTest function
 myTest();
 
+//================== Button Check ===================
+const refreshButton = document.getElementById('refresh');
+
+refreshButton.addEventListener('click', function(event) {
+  alert('Refresh Button has been clicked via app.js!');
+});
+
+
+
+
+
+
+
+/*  ========== PROJECT SANDBOX ======================
+
+Conditionals on input
+
+//Array of possible click outputs
+FreshGame = [X, O, X, O, X, O, X, O, X] --> Maybe think about for every click, iterate through this array and return array[index] to display on the table screen
+
+*/
