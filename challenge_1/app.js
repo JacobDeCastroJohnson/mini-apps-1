3be720754594959c9267aebd2db3c1139735c477
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-09-14 15:37:21 -0700

    Refactor event listener syntax to be DRY

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -22,97 +22,110 @@ let gameValues = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
 
 // var nextClickValue = gameValues.shift(); //removes and returns first item of array
 
+var classClick = document.querySelectorAll('.test');
 
+classClick.forEach(item => {
+  item.addEventListener('click', event => {
+    if (gameValues.length >= 1) {
+      var nextClickValue = gameValues.shift()
+      item.innerHTML = nextClickValue;
+    }
+  })
+})
+
+//================== Tic Tac Toe Rules ===================
+
+//Hard coded
 //Cell 1
-const cellClick = document.getElementById('cellTest');
+// const cellClick = document.getElementById('cellTest');
 
-cellClick.addEventListener('click', function(event) {
+// cellClick.addEventListener('click', function(event) {
 
-  if (gameValues.length >= 1) {
-    var nextClickValue = gameValues.shift()
-    cellClick.innerHTML = nextClickValue;
-  }
-});
+//   if (gameValues.length >= 1) {
+//     var nextClickValue = gameValues.shift()
+//     cellClick.innerHTML = nextClickValue;
+//   }
+// });
 
-//Cell 2
-const cellClickTwo = document.getElementById('cellTest2');
+// //Cell 2
+// const cellClickTwo = document.getElementById('cellTest2');
 
-cellClickTwo.addEventListener('click', function(event) {
-  if (gameValues.length >= 1) {
-    var nextClickValue = gameValues.shift()
-    cellClickTwo.innerHTML = nextClickValue;
-  }
-});
+// cellClickTwo.addEventListener('click', function(event) {
+//   if (gameValues.length >= 1) {
+//     var nextClickValue = gameValues.shift()
+//     cellClickTwo.innerHTML = nextClickValue;
+//   }
+// });
 
-//Cell 3
-const cellClickThree = document.getElementById('cellTest3');
+// //Cell 3
+// const cellClickThree = document.getElementById('cellTest3');
 
-cellClickThree.addEventListener('click', function(event) {
-  if (gameValues.length >= 1) {
-    var nextClickValue = gameValues.shift()
-    cellClickThree.innerHTML = nextClickValue;
-  }
-});
+// cellClickThree.addEventListener('click', function(event) {
+//   if (gameValues.length >= 1) {
+//     var nextClickValue = gameValues.shift()
+//     cellClickThree.innerHTML = nextClickValue;
+//   }
+// });
 
 //Cell 4
-const cellClickFour = document.getElementById('cellTest4');
+// const cellClickFour = document.getElementById('cellTest4');
 
-cellClickFour.addEventListener('click', function(event) {
-  if (gameValues.length >= 1) {
-    var nextClickValue = gameValues.shift()
-    cellClickFour.innerHTML = nextClickValue;
-  }
-});
+// cellClickFour.addEventListener('click', function(event) {
+//   if (gameValues.length >= 1) {
+//     var nextClickValue = gameValues.shift()
+//     cellClickFour.innerHTML = nextClickValue;
+//   }
+// });
 
-//Cell 5
-const cellClickFive = document.getElementById('cellTest5');
+// //Cell 5
+// const cellClickFive = document.getElementById('cellTest5');
 
-cellClickFive.addEventListener('click', function(event) {
-  if (gameValues.length >= 1) {
-    var nextClickValue = gameValues.shift()
-    cellClickFive.innerHTML = nextClickValue;
-  }
-});
+// cellClickFive.addEventListener('click', function(event) {
+//   if (gameValues.length >= 1) {
+//     var nextClickValue = gameValues.shift()
+//     cellClickFive.innerHTML = nextClickValue;
+//   }
+// });
 
-//Cell 6
-const cellClickSix = document.getElementById('cellTest6');
+// //Cell 6
+// const cellClickSix = document.getElementById('cellTest6');
 
-cellClickSix.addEventListener('click', function(event) {
-  if (gameValues.length >= 1) {
-    var nextClickValue = gameValues.shift()
-    cellClickSix.innerHTML = nextClickValue;
-  }
-});
+// cellClickSix.addEventListener('click', function(event) {
+//   if (gameValues.length >= 1) {
+//     var nextClickValue = gameValues.shift()
+//     cellClickSix.innerHTML = nextClickValue;
+//   }
+// });
 
-//Cell 7
-const cellClickSeven = document.getElementById('cellTest7');
+// //Cell 7
+// const cellClickSeven = document.getElementById('cellTest7');
 
-cellClickSeven.addEventListener('click', function(event) {
-  if (gameValues.length >= 1) {
-    var nextClickValue = gameValues.shift()
-    cellClickSeven.innerHTML = nextClickValue;
-  }
-});
+// cellClickSeven.addEventListener('click', function(event) {
+//   if (gameValues.length >= 1) {
+//     var nextClickValue = gameValues.shift()
+//     cellClickSeven.innerHTML = nextClickValue;
+//   }
+// });
 
-//Cell 8
-const cellClickEight = document.getElementById('cellTest8');
+// //Cell 8
+// const cellClickEight = document.getElementById('cellTest8');
 
-cellClickEight.addEventListener('click', function(event) {
-  if (gameValues.length >= 1) {
-    var nextClickValue = gameValues.shift()
-    cellClickEight.innerHTML = nextClickValue;
-  }
-});
+// cellClickEight.addEventListener('click', function(event) {
+//   if (gameValues.length >= 1) {
+//     var nextClickValue = gameValues.shift()
+//     cellClickEight.innerHTML = nextClickValue;
+//   }
+// });
 
-//Cell 9
-const cellClickNine = document.getElementById('cellTest9');
+// //Cell 9
+// const cellClickNine = document.getElementById('cellTest9');
 
-cellClickNine.addEventListener('click', function(event) {
-  if (gameValues.length >= 1) {
-    var nextClickValue = gameValues.shift()
-    cellClickNine.innerHTML = nextClickValue;
-  }
-});
+// cellClickNine.addEventListener('click', function(event) {
+//   if (gameValues.length >= 1) {
+//     var nextClickValue = gameValues.shift()
+//     cellClickNine.innerHTML = nextClickValue;
+//   }
+// });
 
 
 //================== Class Click Check ===================
@@ -123,7 +136,6 @@ cellClickNine.addEventListener('click', function(event) {
 // //   var nextClickValue = gameValues.shift()
 // //   classClick.innerHTML = nextClickValue;
 // // });
-//===========================================================
 
 
 
@@ -132,7 +144,7 @@ cellClickNine.addEventListener('click', function(event) {
 
 
 
-/*  ========== PROJECT SANDBOX ======================
+/*================== PROJECT SANDBOX ======================
 
 Conditionals on input
 
