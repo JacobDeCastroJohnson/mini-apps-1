1432bb5940d11d9866ada4ab7b2e746ddb196b29
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-09-14 14:56:16 -0700

    Implement WET syntax for table event listeners. Functionality OK

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -9,12 +9,122 @@ function myTest() {
 //Invoke the myTest function
 myTest();
 
-//================== Button Check ===================
+//================== Button Click Check ===================
 const refreshButton = document.getElementById('refresh');
 
 refreshButton.addEventListener('click', function(event) {
   alert('Refresh Button has been clicked via app.js!');
 });
+
+//================== Cell Click Check ===================
+
+let gameValues = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+
+// var nextClickValue = gameValues.shift(); //removes and returns first item of array
+
+
+//Cell 1
+const cellClick = document.getElementById('cellTest');
+
+cellClick.addEventListener('click', function(event) {
+
+  if (gameValues.length >= 1) {
+    var nextClickValue = gameValues.shift()
+    cellClick.innerHTML = nextClickValue;
+  }
+});
+
+//Cell 2
+const cellClickTwo = document.getElementById('cellTest2');
+
+cellClickTwo.addEventListener('click', function(event) {
+  if (gameValues.length >= 1) {
+    var nextClickValue = gameValues.shift()
+    cellClickTwo.innerHTML = nextClickValue;
+  }
+});
+
+//Cell 3
+const cellClickThree = document.getElementById('cellTest3');
+
+cellClickThree.addEventListener('click', function(event) {
+  if (gameValues.length >= 1) {
+    var nextClickValue = gameValues.shift()
+    cellClickThree.innerHTML = nextClickValue;
+  }
+});
+
+//Cell 4
+const cellClickFour = document.getElementById('cellTest4');
+
+cellClickFour.addEventListener('click', function(event) {
+  if (gameValues.length >= 1) {
+    var nextClickValue = gameValues.shift()
+    cellClickFour.innerHTML = nextClickValue;
+  }
+});
+
+//Cell 5
+const cellClickFive = document.getElementById('cellTest5');
+
+cellClickFive.addEventListener('click', function(event) {
+  if (gameValues.length >= 1) {
+    var nextClickValue = gameValues.shift()
+    cellClickFive.innerHTML = nextClickValue;
+  }
+});
+
+//Cell 6
+const cellClickSix = document.getElementById('cellTest6');
+
+cellClickSix.addEventListener('click', function(event) {
+  if (gameValues.length >= 1) {
+    var nextClickValue = gameValues.shift()
+    cellClickSix.innerHTML = nextClickValue;
+  }
+});
+
+//Cell 7
+const cellClickSeven = document.getElementById('cellTest7');
+
+cellClickSeven.addEventListener('click', function(event) {
+  if (gameValues.length >= 1) {
+    var nextClickValue = gameValues.shift()
+    cellClickSeven.innerHTML = nextClickValue;
+  }
+});
+
+//Cell 8
+const cellClickEight = document.getElementById('cellTest8');
+
+cellClickEight.addEventListener('click', function(event) {
+  if (gameValues.length >= 1) {
+    var nextClickValue = gameValues.shift()
+    cellClickEight.innerHTML = nextClickValue;
+  }
+});
+
+//Cell 9
+const cellClickNine = document.getElementById('cellTest9');
+
+cellClickNine.addEventListener('click', function(event) {
+  if (gameValues.length >= 1) {
+    var nextClickValue = gameValues.shift()
+    cellClickNine.innerHTML = nextClickValue;
+  }
+});
+
+
+//================== Class Click Check ===================
+// const classClick = document.getElementsByClassName('one');
+// console.log(classClick);
+// // classClick.addEventListener('click', function(event) {
+// //   // alert('This Cell has been clicked via app.js!');
+// //   var nextClickValue = gameValues.shift()
+// //   classClick.innerHTML = nextClickValue;
+// // });
+//===========================================================
+
 
 
 
