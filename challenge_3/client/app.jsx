a8591cbb61d1ec12fb4776a84bba0511ba61f06a
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-09-16 14:40:57 -0700

    Implement Initial Dev Environment for mini app Challenge 3

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    }
+    //THIS BINDING FOR APP METHODS
+  }
+
+  render() {
+//test
+  };
+
+
+}
+
+//ReactDOM Render
+reactDOM.render(<App />, document.getElementById('app'));
 
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//Serve up local files
+// app.use('/static', express.static('public'))
 
 
+app.get('/', (req, res) => {
+  res.send('Hello World Challenge 3!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
